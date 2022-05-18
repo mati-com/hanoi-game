2c87fdbeb4cb1d545fba96a0813c4f210dd4b07a
Matias Ramirez <matiaramirez133@gmail.com>
2022-05-18 16:51:39 -0300

    Mejore y agregue otra forma de hacer el puzzle

--- hanoi-g/hanoi.py
+++ hanoi-g/hanoi.py
@@ -1,4 +1,5 @@
-# Recursive Python function to solve the tower of hanoi
+# Función recursiva de Python para resolver la torre de hanoi
+# Primera forma
 def resultadohanoi (disks, source, middle, destination):
     if disks >= 1: 
         resultadohanoi(disks-1,source, destination, middle)
@@ -6,3 +7,19 @@ def resultadohanoi (disks, source, middle, destination):
         resultadohanoi(disks-1,middle, source, destination)
 
 resultadohanoi(3, 1, 2, 3)
+
+
+# Función recursiva de Python para resolver la torre de hanoi
+# Segunda forma mas compleja
+
+def TorredeHanoi(n , source2, destination2, auxiliary):
+	if n==1:
+		print ("Mover el disco 1 desde la fuente",source2,"al destino",destination2)
+		return
+	TorredeHanoi(n-1, source2, auxiliary, destination2)
+	print ("Mover el disco",n,"de la fuente",source2,"al destino",destination2)
+	TorredeHanoi(n-1, auxiliary, destination2, source2)
+		
+# A, C, B el nombre de los "palitos" del hanoi
+n = 4
+TorredeHanoi (n,'A','B','C')
--- hanoi-g/lamba.py
+++
@@ -1,9 +0,0 @@
-#Sin la utilización de lambda
-def x(y):
-    y = y*2
-    return y 
-print(x(5))
-
-#Utilizo lambda
-x = lambda y: y*2
-print(x(5))
